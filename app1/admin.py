af6399bc0210424d77758545aa3d509797fdb64f
Nasrullah Khan <107609319+nassrkhan@users.noreply.github.com>
2023-06-25 18:53:43 +0500

    user register to admin side

--- app1/admin.py
+++ app1/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(engineerProfile)
+admin.site.register(userProfile)
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
