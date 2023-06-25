8f5037b6549c7428b41e06df7b81ea1239001e30
Nasrullah Khan <107609319+nassrkhan@users.noreply.github.com>
2023-06-25 19:00:13 +0500

    change tables view on admin side

--- app1/admin.py
+++ app1/admin.py
@@ -2,13 +2,20 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(engineerProfile)
-admin.site.register(userProfile)
+# admin.site.register(engineerProfile)
+# admin.site.register(userProfile)
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name","email","subject","message","created_at")
 
+@admin.register(engineerProfile)
+class engineerProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "auth_token","is_verified","created_at","reset_password")
+
+@admin.register(userProfile)
+class userProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "auth_token","is_verified","created_at","reset_password")    
 
     
 @admin.register(engineerDetails)
